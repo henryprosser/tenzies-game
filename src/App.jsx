467a58979c59b511b3cbf289a304dedb6ffdea2f
henryprosser <henryprosser@yahoo.co.uk>
2025-02-05 19:43:24 +0000

    Add Die component and update App layout with dice display

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Die from "../components/Die";
+
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="dice__container">
+        <Die className="die" value="1"></Die>
+        <Die className="die" value="2"></Die>
+        <Die className="die" value="3"></Die>
+        <Die className="die" value="4"></Die>
+        <Die className="die" value="5"></Die>
+        <Die className="die" value="6"></Die>
+        <Die className="die" value="1"></Die>
+        <Die className="die" value="2"></Die>
+        <Die className="die" value="3"></Die>
+        <Die className="die" value="4"></Die>
+      </div>
+    </main>
+  );
 }
 
 export default App;
